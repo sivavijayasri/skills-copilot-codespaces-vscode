d89b40978fdf71bc41b2f9dab20dad7641bbeb45
sivavijayasri <119392018+sivavijayasri@users.noreply.github.com>
2023-12-18 11:04:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/skills-member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'skillsMemberCtrl'
+    };
+}
